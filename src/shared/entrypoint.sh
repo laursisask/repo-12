f339fb05cd0f81203364a789073a10a55dbee9fd
Mark Lopez <m@silvenga.com>
2022-06-16 13:45:08 -0500

    Open sourced the operator images under the Apache 2.0 License. Open source FTW!

--- src/shared/entrypoint.sh
+++ src/shared/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information.
 
 set -x
 mkdir -p $CONTRAST_MOUNT_PATH
--- validate-image.ps1
+++ validate-image.ps1
@@ -1,3 +1,6 @@
+# Contrast Security, Inc licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information.
+
 param (
     [Parameter(Mandatory = $true)]
     [string] $Type,
