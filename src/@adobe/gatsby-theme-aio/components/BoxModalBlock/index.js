5ec22264f4660187bbb3b498c0297fad1e84d534
amandah <amandah@adobe.com>
2023-06-22 10:37:10 -0700

    Fix spacing in boxmodal component and marketplace incolumn

--- src/@adobe/gatsby-theme-aio/components/BoxModalBlock/index.js
+++ src/@adobe/gatsby-theme-aio/components/BoxModalBlock/index.js
@@ -43,7 +43,7 @@ const ModalImage = ({ image, styles }) =>
 
             .gatsby-resp-image-wrapper {
                 max-width: none !important;
-                width: 100% !important;
+                width: 100%;
                 height: 100% !important;
             }
 
